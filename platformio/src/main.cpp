09470acaf5ae7a5b67e1bbded953b78f5abcbfab
Kilian <70379879+kaerbr@users.noreply.github.com>
2025-11-22 21:35:02 +0100

    add open-meteo cert
    + remove unused function declarations from conversions.h

--- platformio/src/main.cpp
+++ platformio/src/main.cpp
@@ -255,7 +255,7 @@ void setup()
   client.setInsecure();
 #elif defined(USE_HTTPS_WITH_CERT_VERIF)
   WiFiClientSecure client;
-  client.setCACert(cert_Sectigo_RSA_Organization_Validation_Secure_Server_CA);
+  client.setCACert(cert_intermediate);
 #endif
 
   // INITIALIZE WEATHER PROVIDER
