e0c8fd6b7d47d92406ebc38480594203aaa20e72
Ken Bloom <kbloom@gmail.com>
2026-03-09 09:11:37 -0400

    Restore temerature unit conversion for indoor temperature.

--- platformio/include/conversions.h
+++ platformio/include/conversions.h
@@ -15,9 +15,37 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#pragma once
+#ifndef __CONVERSIONS_H__
+#define __CONVERSIONS_H__
 
 #include "model/WeatherData.h"
-
-// Function to convert all units in WeatherData object
 void convertWeatherDataUnits(WeatherData &data);
+
+float kelvin_to_celsius(float kelvin);
+float kelvin_to_fahrenheit(float kelvin);
+float celsius_to_kelvin(float celsius);
+float celsius_to_fahrenheit(float celsius);
+
+float meterspersecond_to_feetpersecond(float meterspersecond);
+float meterspersecond_to_kilometersperhour(float meterspersecond);
+float meterspersecond_to_milesperhour(float meterspersecond);
+float meterspersecond_to_knots(float meterspersecond);
+int meterspersecond_to_beaufort(float meterspersecond);
+
+float hectopascals_to_pascals(float hectopascals);
+float hectopascals_to_millimetersofmercury(float hectopascals);
+float hectopascals_to_inchesofmercury(float hectopascals);
+float hectopascals_to_millibars(float hectopascals);
+float hectopascals_to_atmospheres(float hectopascals);
+float hectopascals_to_gramspersquarecentimeter(float hectopascals);
+float hectopascals_to_poundspersquareinch(float hectopascals);
+
+float meters_to_kilometers(float meters);
+float meters_to_miles(float meters);
+float meters_to_feet(float meters);
+
+float millimeters_to_inches(float meters);
+float millimeters_to_centimeters(float meters);
+
+#endif
+
